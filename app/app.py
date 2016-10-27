ce1b000f2139134cacfb05e3222a1d1af2815b86
haewonjeong <d16122926@mydit.ie>
2016-10-27 13:11:51 +0000

    1027

--- app/app.py
+++ app/app.py
@@ -6,15 +6,28 @@ app = Flask(__name__)
 
 def hello():
 
-        return 'Hello Flask from DIT DT228 Program!'
+  return 'Hello Flask from DIT DT228 Program!'
 @app.route('/user/<username>')
 
 def show_user_profile(username):
 
         # show the user profile for that user
 
-            return 'User %s' % username
+    return 'User %s' % username
+
+@app.route('/index')
+def index():
+    user = {'nickname': 'Cloud User'}  # fake user
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>html>
+'''
 
 if __name__ == '__main__':
 
-           app.run(host="0.0.0.0",port=8000, debug=True)
+    app.run(host="0.0.0.0",port=8080, debug=True)
